cb5a9650b86f9081956ac9b457929aae03ea4d78
Hawktuha <raideremil@gmail.com>
2025-03-04 21:54:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -13,4 +13,24 @@ document.getElementById("mogus").onclick = function sigma() {
     setTimeout(() => {
         img.classList.remove("bounce");
     }, 100); 
+
+    if (count === 100) {
+        activateBonus();
+    }
+
 };
+
+function activateBonus() {
+    let img = document.getElementById("mogus");
+
+            // Create a new <a> element
+            let link = document.createElement("a");
+            link.href = "https://www.youtube.com/watch?v=l6hzRU2d7c0"; // Change this to your desired link
+            link.target = "_blank"; // Opens in a new tab
+            link.id = "mogus-link";
+
+            // Replace the image with the link containing the image
+            img.parentNode.replaceChild(link, img);
+            link.appendChild(img);
+    
+}
