8b767a6f70b336c35ae386ff30815c5921ed2648
Hawktuha <raideremil@gmail.com>
2025-03-04 21:39:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-let count = 0
+let count = 0;
 
-document.getElementById("mogus").onclick = function sigma () {
-    count = count + 1
-    document.getElementById("count-el").innerHTML = (count)
-    console.log (count)
-}
+document.getElementById("mogus").onclick = function sigma() {
+    count += 1;
+    document.getElementById("count-el").innerHTML = count;
+    console.log(count);
+
+    let img = document.getElementById("mogus");
+    
+    img.classList.add("bounce");
+
+   
+    setTimeout(() => {
+        img.classList.remove("bounce");
+    }, 100); 
+};
